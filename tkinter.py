b465285a3924e3cafd4acbbba13043c29bd9e90d
larirodrigue <larissarodriguesn@hotmail.com>
2023-02-05 22:14:39 -0300

    add firstcod

---
+++ tkinter.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+import random
+
+root = tk.Tk()
+root.geometry('200x200')
+
+def hover(event):
+    x = random.randint(0, 180)
+    y = random.randint(0, 180)
+    nao.place(x=x, y=y)
+
+def messages():
+    message = tk.Label(root, text='toba')
+    message.place(x=70, y=115, relx=0, rely=0)
+
+pergunta = tk.Label(root, text='Você é corno?')
+pergunta.pack(anchor='n', pady=20)
+
+nao = tk.Button(root, text='Não')
+nao.place(x='99', y='80')
+nao.bind('<Enter>', hover)
+
+sim = tk.Button(root, text='Sim', command=messages)
+sim.place(x=25, y=80, relx=0, rely=0)
+
+root.mainloop()
